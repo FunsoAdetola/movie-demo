8333cc48ac0f85eef6273142f3d6a2e1f8ba5335
Funsoadetola <{funsoadetola}@gmail.com>
2020-12-12 17:58:44 +0100

    Updates

---
+++ precache-manifest.6b84ec1b3720b8a2e0fb511a044b73b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d8bdcde05d3f1bb68df4952f9a0f00b",
+    "url": "/movie-demo/index.html"
+  },
+  {
+    "revision": "8c659225366737e34baa",
+    "url": "/movie-demo/static/css/main.4ed27d6c.chunk.css"
+  },
+  {
+    "revision": "6c15671d0f4fa6b8a731",
+    "url": "/movie-demo/static/js/2.c685ea0d.chunk.js"
+  },
+  {
+    "revision": "8c659225366737e34baa",
+    "url": "/movie-demo/static/js/main.e475acf9.chunk.js"
+  },
+  {
+    "revision": "eb4c0183fd90821bf344",
+    "url": "/movie-demo/static/js/runtime~main.fa505da1.js"
+  }
+]);
